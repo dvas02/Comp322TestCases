176222f45a56cfd4e962900b1c6a6e1576cd5c23
Dov Vas <115668357+dvas02@users.noreply.github.com>
2024-01-23 17:59:18 -0500

    Create A1.cpp
    Description of how test cases should look

---
+++ A1.cpp
@@ -0,0 +1,38 @@
+/*
+  Steps:
+    1. Add the following code to the top of your file
+      #include <iostream>
+      using namespace std;
+      unordered_map<int, string> runTestCases();
+
+    2. Add the runTestCases() function below to the end of your code
+
+  Adding test cases:
+    - Make sure to follow the general structure of the previous test cases
+    - Make sure your test case works on your device before adding it here
+    - Add your test case to the end of the cases
+
+    General format:
+      // Test Case 1
+      // Test description...
+      expected = ... ;
+      actual = function(){}
+      if(actual == expected){
+        testCases[1] = "PASSED";
+      }
+      else{
+        testCases[1] = "FAILED";
+        cout << "Test 1 Failed:" << endl;
+        cout << "Expected: " + expected << endl;
+        cout << "Actual: " + actual << endl;
+      }
+*/
+
+
+
+
+
+
+
+
+
