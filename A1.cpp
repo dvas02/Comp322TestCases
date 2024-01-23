c993825caee52158b3fb42e902bf986aec7faa2d
Dov Vas <115668357+dvas02@users.noreply.github.com>
2024-01-23 18:11:12 -0500

    Update A1.cpp
    Test 1

--- A1.cpp
+++ A1.cpp
@@ -15,21 +15,54 @@
     General format:
       // Test Case 1
       // Test description...
-      expected = ... ;
-      actual = function(){}
-      if(actual == expected){
-        testCases[1] = "PASSED";
-      }
-      else{
+      try{
+        expected = ... ;
+        actual = function(){}
+        if(actual == expected){
+          testCases[1] = "PASSED";
+        }
+        else{
         testCases[1] = "FAILED";
         cout << "Test 1 Failed:" << endl;
         cout << "Expected: " + expected << endl;
         cout << "Actual: " + actual << endl;
+        }
       }
+      catch (const exception &e) {
+        testCases[1] = "FAILED";
+        cout << "Test 1 Failed:" << endl;
+        cout << "Unexpected Exception: " << e.what() << endl;
+    }
+
 */
 
 
+// Test Cases Function
+unordered_map<int, string> runTestCases(){
+  unordered_map<int, string> testCases;
 
+  // Test Case 1: Male, Age: 25, Sedentary Lifestyle
+    // The expected result is 2400 calories.
+    try {
+        int expected = 2400;
+        int actual = get_daily_calories(25, "male", "sedentary");
+
+        if (actual == expected) {
+            testCases[1] = "PASSED";
+        } else {
+            testCases[1] = "FAILED";
+            cout << "Test 1 Failed:" << endl;
+            cout << "Expected: " << expected << endl;
+            cout << "Actual: " << actual << endl;
+        }
+    } catch (const exception &e) {
+        testCases[1] = "FAILED";
+        cout << "Test 1 Failed:" << endl;
+        cout << "Unexpected Exception: " << e.what() << endl;
+    }
+
+  return testCases;
+}
 
 
 
